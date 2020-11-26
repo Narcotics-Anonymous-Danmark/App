de43ed45e2076897941dfc2953d99c5b35db9916
Jonas J <jonasdev2015@gmail.com>
2020-11-26 22:52:31 +0100

    Added menu items - pointing at "coming-soon".

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,82 +7,117 @@ import { TranslateService } from '@ngx-translate/core';
 import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-  public appPages = [
-    {
-      title: 'HOME',
-      url: 'home',
-      icon: 'home'
-    },
-    {
-      title: 'SETTINGS',
-      url: 'settings',
-      icon: 'settings'
-    },
-    {
-      title: 'MAP_SEARCH',
-      url: 'map-search',
-      icon: 'map'
-    },
-    {
-      title: 'LOCATIONSEARCH',
-      url: 'location-search',
-      icon: 'search'
-    },
-    {
-      title: 'LISTFULL',
-      url: 'listfull',
-      icon: 'list'
-    },
-    {
-      title: 'VIRTUAL_MEETINGS',
-      url: 'virt-tabs',
-      icon: 'globe-outline'
-    },
-    {
-      title: 'DOIHAVETHEBMLT',
-      url: 'do-i-have-the-bmlt',
-      icon: 'help'
-    },
-    {
-      title: 'CONTACT',
-      url: 'contact',
-      icon: 'person'
+    public appPages = [
+        {
+            title: 'HOME',
+            url: 'home',
+            icon: 'home'
+        },
+        //{
+        //  title: 'MAP_SEARCH',
+        //  url: 'map-search',
+        //  icon: 'map'
+        //},
+        {
+            title: 'JFT',
+            url: 'coming-soon',
+            icon: 'albums'
+        },
+        {
+            title: 'LOCATIONSEARCH',
+            url: 'location-search',
+            icon: 'search'
+        },
+        {
+            title: 'LISTFULL',
+            url: 'listfull',
+            icon: 'list'
+        },
+        {
+            title: 'NACC',
+            url: 'coming-soon',
+            icon: 'hourglass'
+        },
+        //{
+        //  title: 'VIRTUAL_MEETINGS',
+        //  url: 'virt-tabs',
+        //  icon: 'globe-outline'
+        //},
+        //{
+        //  title: 'DOIHAVETHEBMLT',
+        //  url: 'do-i-have-the-bmlt',
+        //  icon: 'help'
+        //},
+        {
+            title: 'NEWS',
+            url: 'coming-soon',
+            icon: 'bulb'
+        },
+        {
+            title: 'EVENTS',
+            url: 'coming-soon',
+            icon: 'calendar'
+        },
+        //{
+        //    title: 'AUDIOBOOKS',
+        //    url: 'audiobooks',
+        //    icon: 'book'
+        //},
+        {
+            title: 'SPEAKS',
+            url: 'coming-soon',
+            icon: 'chatbox-ellipses'
+        },
+        {
+            title: 'GRC',
+            url: 'coming-soon',
+            icon: 'reader'
+        },
+        {
+            title: 'SETTINGS',
+            url: 'settings',
+            icon: 'settings'
+        },
+        {
+            title: 'CONTACT',
+            url: 'contact',
+            icon: 'person'
+        }
+    ];
+
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private translate: TranslateService,
+        private storage: Storage
+    ) {
+        this.initializeApp();
+        this.translate.setDefaultLang('en');
+        storage.get('language').then((value) => {
+            if (value) {
+                this.translate.use(value);
+            } else {
+                this.translate.use('en');
+                storage.set('language', 'en');
+            }
+        });
     }
-  ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private translate: TranslateService,
-    private storage: Storage
-  ) {
-    this.initializeApp();
-    this.translate.setDefaultLang('en');
-    storage.get('language').then((value) => {
-      if (value) {
-        this.translate.use(value);
-      } else {
-        this.translate.use('en');
-        storage.set('language', 'en');
-      }
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    ngOnInit() {
 
-  ngOnInit() {
-
-  }
+    }
 }
