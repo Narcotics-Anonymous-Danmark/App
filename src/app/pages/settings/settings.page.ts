eb2d99e4f7ed32a36b54f22ea89b4189c0cc3399
Jonas Servicekonto <jonas.servicekonto@gmail.com>
2020-12-09 13:59:28 +0100

    Settings: First day of week sun/mon, initial commit. Settings: Show own region on top, initial commit.

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -3,55 +3,71 @@ import { Storage } from '@ionic/storage';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+    selector: 'app-settings',
+    templateUrl: './settings.page.html',
+    styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
 
-  language: string;
-  theme: string;
-  searchRange: number;
+    language: string;
+    firstdayofweek: string;
+    theme: string;
+    searchRange: number;
 
-  constructor(
-    private storage: Storage,
-    private translate: TranslateService
-  ) {
+    constructor(
+        private storage: Storage,
+        private translate: TranslateService
+    ) {
 
-  }
+    }
 
-  ngOnInit() {
-    this.storage.get('language')
-      .then(langValue => {
-        if (langValue) {
-          this.language = langValue;
-        } else {
-          this.language = 'en';
-        }
-      });
+    ngOnInit() {
+        this.storage.get('language')
+            .then(langValue => {
+                if (langValue) {
+                    this.language = langValue;
+                } else {
+                    this.language = 'en';
+                }
+            });
 
-    this.storage.get('searchRange')
-      .then(searchValue => {
-        if (searchValue) {
-          this.searchRange = searchValue;
-        } else {
-          this.searchRange = 25;
-        }
-      });
-  }
+        this.storage.get('firstday')
+            .then(firstdayValue => {
+                if (firstdayValue) {
+                    this.firstdayofweek = firstdayValue;
+                } else {
+                    this.firstdayofweek = 'su';
+                }
+            });
 
-  selectLanguage() {
-    this.storage.set('language', this.language);
-    this.translate.setDefaultLang(this.language);
-    this.translate.use(this.language);
-  }
+        this.storage.get('searchRange')
+            .then(searchValue => {
+                if (searchValue) {
+                    this.searchRange = searchValue;
+                } else {
+                    this.searchRange = 25;
+                }
+            });
+    }
 
-  selectTheme() {
-    this.storage.set('theme', this.theme);
-  }
+    selectLanguage() {
+        this.storage.set('language', this.language);
+        this.translate.setDefaultLang(this.language);
+        this.translate.use(this.language);
+    }
 
-  selectSearchRange() {
-    this.storage.set('searchRange', this.searchRange);
-  }
+    selectFirstDayOfWeek() {
+        this.storage.set('firstday', this.firstdayofweek);
+        //this.translate.setDefaultLang(this.language);
+        //this.translate.use(this.language);
+    }
+
+    selectTheme() {
+        this.storage.set('theme', this.theme);
+    }
+
+    selectSearchRange() {
+        this.storage.set('searchRange', this.searchRange);
+    }
 
 }
