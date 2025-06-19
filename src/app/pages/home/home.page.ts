d018b6d4b3365ea67e1fa5513d61a68a0dab514c
Madalin <iu.naestformand@nadanmark.dk>
2025-06-19 22:16:35 +0200

    feat: start on notifications

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var cordova: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -10,6 +12,11 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    cordova.plugins.notification.local.schedule({
+        title: 'Test',
+        text: 'This is a very important test',
+        trigger: { at: new Date(2025, 5, 19, 22, 10, 0) }
+    });
   }
 
 }
